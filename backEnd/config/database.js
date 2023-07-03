9acad56032e64f7cf15b5ea8b35d379ba98d3308
Rahul Kumar Saw <2021chb1052@iitrpr.ac.in>
2023-07-04 03:23:57 +0530

    update database.js

--- backEnd/config/database.js
+++ backEnd/config/database.js
@@ -4,15 +4,14 @@ const username = process.env.MONGO_USER;
 const password = process.env.MONGO_PASS;
 const database = process.env.MONGO_DATABASE;
 
-const connectDatabase = () => {
-	mongoose
-		.connect("mongodb+srv://" + username + ":" + password + "@cluster0.3uwjrdn.mongodb.net/" + database + "?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
-		.then((data) => {
-			console.log(`MongoDb connected with server ${data.connection.host}`);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+const connectDatabase = async () => {
+	try {
+		const conn = await mongoose.connect("mongodb+srv://" + username + ":" + password + "@cluster0.3uwjrdn.mongodb.net/" + database + "?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDatabase;
