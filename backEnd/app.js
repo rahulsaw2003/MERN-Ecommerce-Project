ed180f5c282cf0f3e9cd76b2320c27dc3cd2c8a9
Rahul Kumar Saw <2021chb1052@iitrpr.ac.in>
2023-07-04 02:58:34 +0530

    update  gitignore

--- backEnd/app.js
+++ backEnd/app.js
@@ -32,7 +32,7 @@ app.use("/api/v1", payment);
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+	res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 // Middleware for Errors
